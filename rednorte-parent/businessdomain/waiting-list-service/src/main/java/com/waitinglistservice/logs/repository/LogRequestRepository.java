4838aae3e1ee79a4e4a0546ace4219a00e9a8be8
Benja Arellano <be.arellanor@duocuc.cl>
2026-05-13 19:38:26 -0400

    logs lista de espera listos

---
+++ rednorte-parent/businessdomain/waiting-list-service/src/main/java/com/waitinglistservice/logs/repository/LogRequestRepository.java
@@ -0,0 +1,20 @@
+package com.waitinglistservice.logs.repository;
+
+import com.waitinglistservice.logs.entity.LogRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface LogRequestRepository extends JpaRepository<LogRequest, Long> {
+
+    List<LogRequest> findByMicroservicioOrderByFechaDesc(String microservicio);
+
+    List<LogRequest> findByFechaBetween(LocalDateTime inicio, LocalDateTime fin);
+
+    List<LogRequest> findTop20ByOrderByFechaDesc();
+
+    @Query("SELECT AVG(l.tiempoRespuesta) FROM LogRequest l WHERE l.endpoint = :endpoint")
+    Double getTiempoPromedioByEndpoint(@Param("endpoint") String endpoint);
+}
